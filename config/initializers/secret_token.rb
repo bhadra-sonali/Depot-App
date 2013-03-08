98e8f900aabd30cf1c1e6d3918a9e6fbb88afebc
cooki <cooki@cooki-Compaq-Presario-CQ40-Notebook-PC.(none)>
2013-03-08 13:09:40 +0530

    myBackup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '41e73d30e90b13d8e2579f88bd171c163df897a9ed192e2e2ccdd3fa676ce784c938170cdf114af246c2021ffd8b7737d8ddb895c29c899f529d49441f14b1ee'
